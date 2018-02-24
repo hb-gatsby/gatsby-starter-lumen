c5f3537cf7d26d789854d4d6ee16c2308fbf4944
Mario Lopez <mario@techmunchies.net>
2018-02-23 21:10:01 -0500

    Fixed code style

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Helmet from "react-helmet";
+import Helmet from 'react-helmet';
 
 class SEO extends React.Component {
   render() {
@@ -21,70 +21,70 @@ class SEO extends React.Component {
       title = config.title;
       description = config.subtitle;
     }
-    const realPrefix = config.pathPrefix === "/" ? "" : config.pathPrefix;
+    const realPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix;
     const image = config.shareImage !== '' ? config.url + realPrefix + config.shareImage : '';
     const blogURL = config.url + config.pathPrefix;
     const schemaOrgJSONLD = [{
-      "@context": "http://schema.org",
-      "@type": "WebSite",
+      '@context': 'http://schema.org',
+      '@type': 'WebSite',
       url: blogURL,
       name: title,
-      alternateName: config.titleAlt ? config.titleAlt : ""
+      alternateName: config.titleAlt ? config.titleAlt : ''
     }];
     if (postSEO) {
       schemaOrgJSONLD.push([{
-          "@context": "http://schema.org",
-          "@type": "BreadcrumbList",
-          itemListElement: [{
-            "@type": "ListItem",
-            position: 1,
-            item: {
-              "@id": postURL,
-              name: title,
-              image
-            }
-          }]
+        '@context': 'http://schema.org',
+        '@type': 'BreadcrumbList',
+        itemListElement: [{
+          '@type': 'ListItem',
+          position: 1,
+          item: {
+            '@id': postURL,
+            name: title,
+            image
+          }
+        }]
+      },
+      {
+        '@context': 'http://schema.org',
+        '@type': 'BlogPosting',
+        url: blogURL,
+        name: title,
+        alternateName: config.titleAlt ? config.titleAlt : '',
+        headline: title,
+        image: {
+          '@type': 'ImageObject',
+          url: image
         },
-        {
-          "@context": "http://schema.org",
-          "@type": "BlogPosting",
-          url: blogURL,
-          name: title,
-          alternateName: config.titleAlt ? config.titleAlt : '',
-          headline: title,
-          image: {
-            "@type": "ImageObject",
-            url: image
-          },
-          description
-        }
+        description
+      }
       ]);
     }
     return (
       <Helmet>
         { /* General tags */ }
-        <meta name = "description" content = { description } />
-        { image !== '' ? <meta name = "image" content = { image } /> : null }
+        <meta name="description" content={description} />
+        { image !== '' ? <meta name="image" content={image} /> : null }
 
         { /* Schema.org tags */ }
-        <script type = "application/ld+json">
+        <script type="application/ld+json">
           { JSON.stringify(schemaOrgJSONLD) }
         </script>
 
         { /* OpenGraph tags */ }
-        <meta property = "og:url" content = { postSEO ? postURL : blogURL } />
-        { postSEO ? <meta property = "og:type" content = "article" /> : null }
-        <meta property = "og:title" content = { title } />
-        <meta property = "og:description" content = { description } />
-        { image !== '' ? <meta property = "og:image" content = { image } /> : null }
-        <meta property = "fb:app_id" content = { config.facebookAppID ? config.facebookAppID : '' } />
+        <meta property="og:url" content={postSEO ? postURL : blogURL} />
+        { postSEO ? <meta property="og:type" content="article" /> : null }
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        { image !== '' ? <meta property="og:image" content={image} /> : null }
+        <meta property="fb:app_id" content={config.facebookAppID ? config.facebookAppID : ''} />
 
         { /* Twitter Card tags */ }
-        <meta name = "twitter:card" content = "summary_large_image" / >
-        <meta name = "twitter:creator" content = { config.author.twitter !== '#' ? config.author.twitter : '' } />
-        <meta name = "twitter:title" content = { title } />
-        <meta name = "twitter:description" content = { description } />
-        { image !== '' ? <meta name = "twitter:image" content = { image } /> : null }
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content={config.author.twitter !== '#' ? config.author.twitter : ''} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        { image !== '' ? <meta name="twitter:image" content={image} /> : null }
       </Helmet>
     );
   }
