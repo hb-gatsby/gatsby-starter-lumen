13e2de414db26185d6d244802c6f8b8bb6048cee
Stephanie Chau <schau@uc.cl>
2020-02-17 15:41:58 -0300

    Added socialImage prop

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import moment from 'moment';
 import { Link } from 'gatsby';
+import get from 'lodash/get';
 import type { Edges } from '../../types';
 import styles from './Feed.module.scss';
 
@@ -9,27 +10,32 @@ type Props = {
   edges: Edges
 };
 
-const Feed = ({ edges }: Props) => (
-  <div className={styles['feed']}>
-    {edges.map((edge) => (
-      <div className={styles['feed__item']} key={edge.node.fields.slug}>
-        <div className={styles['feed__item-meta']}>
-          <time className={styles['feed__item-meta-time']} dateTime={moment(edge.node.frontmatter.date).format('MMMM D, YYYY')}>
-            {moment(edge.node.frontmatter.date).format('MMMM YYYY')}
-          </time>
-          <span className={styles['feed__item-meta-divider']} />
-          <span className={styles['feed__item-meta-category']}>
-            <Link to={edge.node.fields.categorySlug} className={styles['feed__item-meta-category-link']}>{edge.node.frontmatter.category}</Link>
-          </span>
+const Feed = ({ edges }: Props) => {
+  const socialImage = get(edges[0], 'node.frontmatter.socialImage', null);
+  console.log(edges);
+  return (
+    <div className={styles['feed']}>
+      {edges.map((edge) => (
+        <div className={styles['feed__item']} key={edge.node.fields.slug}>
+          <div className={styles['feed__item-meta']}>
+            <time className={styles['feed__item-meta-time']} dateTime={moment(edge.node.frontmatter.date).format('MMMM D, YYYY')}>
+              {moment(edge.node.frontmatter.date).format('MMMM YYYY')}
+            </time>
+            <span className={styles['feed__item-meta-divider']} />
+            <span className={styles['feed__item-meta-category']}>
+              <Link to={edge.node.fields.categorySlug} className={styles['feed__item-meta-category-link']}>{edge.node.frontmatter.category}</Link>
+            </span>
+          </div>
+          <h2 className={styles['feed__item-title']}>
+            <Link className={styles['feed__item-title-link']} to={edge.node.fields.slug}>{edge.node.frontmatter.title}</Link>
+          </h2>
+          {socialImage && <img alt="" src={socialImage} />}
+          <p className={styles['feed__item-description']}>{edge.node.frontmatter.description}</p>
+          <Link className={styles['feed__item-readmore']} to={edge.node.fields.slug}>Read</Link>
         </div>
-        <h2 className={styles['feed__item-title']}>
-          <Link className={styles['feed__item-title-link']} to={edge.node.fields.slug}>{edge.node.frontmatter.title}</Link>
-        </h2>
-        <p className={styles['feed__item-description']}>{edge.node.frontmatter.description}</p>
-        <Link className={styles['feed__item-readmore']} to={edge.node.fields.slug}>Read</Link>
-      </div>
-    ))}
-  </div>
-);
+      ))}
+    </div>
+  );
+};
 
 export default Feed;
