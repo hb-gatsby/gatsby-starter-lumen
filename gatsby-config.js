08b08bbc22e4d46441449f71d24f8a6dfd22d002
morochroyce <morochroyce@gmail.com>
2020-02-17 12:39:21 -0500

    ipfs plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ const siteConfig = require('./config.js');
 const postCssPlugins = require('./postcss-config.js');
 
 module.exports = {
-  pathPrefix: siteConfig.pathPrefix,
+  pathPrefix: '__GATSBY_IPFS_PATH_PREFIX__',
   siteMetadata: {
     url: siteConfig.url,
     title: siteConfig.title,
@@ -15,6 +15,7 @@ module.exports = {
     author: siteConfig.author
   },
   plugins: [
+    'gatsby-plugin-ipfs',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
