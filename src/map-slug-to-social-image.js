140d6cef062d50521aa4005a62b51e7e8c38c3df
mateuszJS <mate.walendzik@gmail.com>
2020-03-06 12:20:44 +0100

    add default metaimage

--- src/map-slug-to-social-image.js
+++ src/map-slug-to-social-image.js
@@ -1,7 +1,7 @@
 import ReleaseNodeImg from '../static/media/Release Notes.png';
-import TerminalLogo from '../static/photo.jpg';
+import TerminalMetaimage from '../static/media/metaimage.jpg';
 
 export default {
   'Release-update-1': ReleaseNodeImg,
-  default: TerminalLogo,
+  default: TerminalMetaimage,
 };
