9fe79bb19a68902c517bd6b245ff211662ac1270
Brett Shear <43047258+Blockchainbrett@users.noreply.github.com>
2020-03-24 21:58:20 -0400

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   copyright: '© 2020 Deployed on IPFS with Terminal, — Built with Gatsby',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-144542192-4',
   useKatex: false,
   menu: [
     {
