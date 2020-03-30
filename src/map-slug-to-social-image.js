be9e7755ada14add92fcd70b08d464818710dcd4
Mateusz Walendzik <mate.walendzik@gmail.com>
2020-03-30 22:10:30 +0200

    update image

--- src/map-slug-to-social-image.js
+++ src/map-slug-to-social-image.js
@@ -1,7 +1,7 @@
 import ReleaseNodeImg from '../static/media/Release Notes.png';
-import TerminalMetaimage from '../static/media/metaimage.jpg';
+import FleekMetaimage from '../static/media/twitter-meta-image.png';
 
 export default {
   'Release-update-1': ReleaseNodeImg,
-  default: TerminalMetaimage,
+  default: FleekMetaimage,
 };
